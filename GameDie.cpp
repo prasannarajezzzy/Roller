50ee51ef23999cb572d825a8edebb0b1ad95e67b
V.Prasanna <30752161+prasannarajezzzy@users.noreply.github.com>
2023-02-21 10:40:08 -0800

    Changes in GameDice.cpp

--- GameDie.cpp
+++ GameDie.cpp
@@ -5,37 +5,45 @@
 #include <ctime>
 
 // class constructor that seeds the random number generator
-GameDie::GameDie() {
+GameDie::GameDie()
+{
     srand(time(NULL));
     roll_counter.resize(FACES);
 
-    for (int i = 0; i < FACES; i++ )
-      roll_counter[i] = 0;
+    for (int i = 0; i < FACES; i++)
+        roll_counter[i] = 0;
 }
 
 // overloaded constructor
-GameDie::GameDie(unsigned int num) {
+GameDie::GameDie(unsigned int num)
+{
     srand(time(NULL));
-    if ( num == 0 ) {
+    if (num == 0)
+    {
         roll_counter.resize(FACES);
-    } else {
+    }
+    else
+    {
         roll_counter.resize(num);
     }
-    for (int i = 0; i < FACES; i++) {
+    for (int i = 0; i < FACES; i++)
+    {
         roll_counter[i] = 0;
     }
 }
 
 // generate a random number between 1-n where n is the counter size
 // (inclusive) and return it
-int GameDie::roll() {
-    int roll = rand_r() % roll_counter.size();
+int GameDie::roll()
+{
+    int roll = rand() % roll_counter.size();
     roll_counter[roll]++;
     return roll + 1;
 }
 
 // return the count of how many times each face has been rolled, as a vector
 // where each face's count is at index face-1 (i.e. Face 1 is at index 0)
-vector <int> GameDie::get_distribution() {
+vector<int> GameDie::get_distribution()
+{
     return roll_counter;
 }
--- main.cpp
+++ main.cpp
@@ -6,8 +6,10 @@
 using std::cout;
 using std::endl;
 
-int main(int argc, char *argv[]) {
-  if ( argc != 2 || std::atoi(argv[1]) < 1 ){
+int main(int argc, char *argv[])
+{
+  if (argc != 2 || std::atoi(argv[1]) < 1)
+  {
     cout << "Incorrect command.\n"
          << "Format: ./Roller <n>\n"
          << "--------------------\n"
@@ -15,8 +17,9 @@ int main(int argc, char *argv[]) {
          << "--------------------\n"
          << "<n> - Required; a number 1 or greater representing the number\n"
          << "      of faces on the die being rolled\n";
-  } 
-  else {
+  }
+  else
+  {
     int faces = std::atoi(argv[1]);
     GameDie die(faces);
     cout << die.roll() << endl;
